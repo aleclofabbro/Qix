ee07969b2e45783ef3816ed29370a9ef58d35740
aleclofabbro <alessandro.giansanti@gmail.com>
2015-08-12 00:31:29 +0200

    OK ! LENS !!

--- src/com/aleclofabbro/qix/JsonSubject.js
+++ src/com/aleclofabbro/qix/JsonSubject.js
@@ -1,63 +1,49 @@
-define(['rx'],
-  function(Rx) {
-
-    var JsonSubject = function(parent, path) {
+define(['rx', 'ramda'],
+  function(Rx, R) {
+    window.R = R;
+    var lens = function(path) {
+      var root = this;
+      var _set = R.assocPath(path);
+      var _get = R.path(path);
       var _observers = [];
+      var _root_ver = Rx.Observer.create(function(v) {
+        lens_subj.value = v;
+        for (var i = 0, os = _observers, len = os.length; i < len; i++) {
+          os[i].onNext(v);
+        }
+      });
 
       var ver = Rx.Observer.create(function(v) {
-        if (this.value === v)
-          return;
-        if (parent && this.value !== v) {
-          this.value = v;
-          parent.set(v, path);
-        } else {
-          for (var i = 0, os = _observers, len = os.length; i < len; i++) {
-            os[i].onNext(v);
-          }
-        }
+        var _root_val = _set(v, root.value || {});
+        root.onNext(_root_val);
       });
       var ble = Rx.Observable.create(function(obs) {
         _observers.push(obs);
+        obs.onNext(lens_subj.value);
         return function() {
-          _observers.splice(_observers.inndexOf(obs), 1);
+          _observers.splice(_observers.indexOf(obs), 1);
         };
       });
-      var jssubj = Rx.Subject.create(ver, ble);
-      jssubj.set = function(v, sub_path) {
-        if (parent)
-          parent.set(v, (path || []).concat(sub_path));
-        else {
-          var _val = this.value = (this.value || {});
-          for (var i = 0; i < sub_path.length - 1; i++) {
-            _val[sub_path[i]] = _val = (_val[sub_path[i]] || {});
-          }
-          _val[sub_path[sub_path.length - 1]] = v;
-          ver.onNext(this.value);
-        }
+
+      var lens_subj = Rx.Subject.create(ver, ble);
+
+      lens_subj.path = path;
+
+      lens_subj.lens = function(_sub_path) {
+        return root.lens(path.concat(_sub_path));
       };
-      // jssub.getPath = function() {
-      //   if (this.parent)
-      //     return this.parent.getPath().concat(path);
-      //   else return [];
-      // };
-      jssubj.sub = function(sub_path) {
-        var ssub = new JsonSubject(this, sub_path);
-        this
-          .map(function(v) {
-            var _val = v;
-            for (var i = 0; i < sub_path.length; i++) {
-              if (_val === null || _val === undefined)
-                break;
-              _val = _val[sub_path[i]];
-            }
-            return _val;
-          })
-          .subscribe(ssub);
-        return ssub;
-      };
-      return jssubj;
+      root
+        .map(function() {
+          return _get(root.value);
+        })
+        .subscribe(_root_ver);
+      return lens_subj;
     };
 
 
-    return JsonSubject;
+    return function(v) {
+      var _root = new Rx.BehaviorSubject(v);
+      _root.lens = lens.bind(_root);
+      return _root;
+    };
   });
