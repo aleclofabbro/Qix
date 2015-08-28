fadec4146d62b386c45e5ef1349a492a54f5c602
aleclofabbro <alessandro.giansanti@gmail.com>
2015-08-28 19:09:41 +0200

    .

--- test/js/bla/ciccio.js
+++ test/js/bla/ciccio.js
@@ -4,19 +4,13 @@ define([
   function(templ) {
     "use strict";
     return {
-      control: function(el, def) {
-        // console.log('bind:', el);
-        var _inn = el.innerHTML;
-        el.innerHTML = '';
-        templ.compileTo(el, el.$qix.$spawn(true), function() {
-          var text_node = document.createTextNode('');
-          el.$qix.$broadcaster.subscribe(function(v) {
-            text_node.textContent = '::::' + def.attr.value + '::' + v + '::' + _inn;
-          });
+      control: function(ctx, spawn) {
+        //var subctx = spawn();
+        var _inn = ctx.elem.innerHTML;
+        ctx.elem.innerHTML = '';
+        templ.compileTo(ctx.elem, ctx, function(sub_elem) {
 
-          el.insertBefore(text_node, el.childNodes[0])
         });
-        return {};
       }
     };
   });
--- test/js/bla/col.js
+++ test/js/bla/col.js
@@ -3,17 +3,12 @@ define([],
     "use strict";
     var _def_colors = ['red', 'blue'];
     return {
-      control: function(el, my_def) {
+      control: function(ctx) {
         var b = true,
           _col_arr = _def_colors;
-        el.$qix.$broadcaster.subscribe(function(v) {
+        ctx.myReceiver().subscribe(function() {
           el.style.color = _col_arr[Number(b = !b)];
         });
-        return {
-          set: function(arr) {
-            _col_arr = arr;
-          }
-        };
       }
     };
   });
