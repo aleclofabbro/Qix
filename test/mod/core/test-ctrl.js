b15d29c6f25f549f10a5c9cc090106d54c5b2b95
alessandro.giansanti <alessandro.giansanti@sistemaits.com>
2015-10-22 18:43:53 +0200

    bettert test

--- test/mod/core/test-ctrl.js
+++ test/mod/core/test-ctrl.js
@@ -3,6 +3,7 @@ define([],
     "use strict";
     return {
       x: function $qix(elem, binders, link) {
+        elem.innerText = 'controller [x] set this text here';
         return {
           x: {
             elem: elem,
@@ -12,6 +13,9 @@ define([],
         };
       },
       y: function $qix(elem, binders, link) {
+        elem.style.color = 'blue';
+        elem.title = 'controller [y] bound a click event here';
+        elem.addEventListener('click', alert.bind(window, 'controller [y] alerts!'));
         return {
           y: {
             elem: elem,
